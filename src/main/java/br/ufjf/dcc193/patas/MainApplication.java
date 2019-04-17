5d7d463213327ae5753199bcc2cc1fb29125de22
Marcos Aquino <marcos-aquino@live.com>
2019-04-17 18:38:29 -0300

    Criação do projeto

--- src/main/java/br/ufjf/dcc193/patas/MainApplication.java
+++ src/main/java/br/ufjf/dcc193/patas/MainApplication.java
@@ -9,7 +9,7 @@ public class MainApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 
-		System.out.println("DEU de novo!");
+		System.out.println("Criação do projeto!");
 	}
 
 }
